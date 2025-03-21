40338e1ec89ec27cc08a0aee1263e1681bfabe3c
Eugene Sukhominskiy <yevhenii.sukhominskiy@gmail.com>
2025-03-21 22:08:47 +0200

    Added main repositories

---
+++ src/main/java/by/eugene/bookforge/repositories/GenreRepository.java
@@ -0,0 +1,10 @@
+package by.eugene.bookforge.repositories;
+
+import by.eugene.bookforge.models.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+
+}
---
+++ src/main/java/by/eugene/bookforge/repositories/MemberRepository.java
@@ -0,0 +1,10 @@
+package by.eugene.bookforge.repositories;
+
+import by.eugene.bookforge.models.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
