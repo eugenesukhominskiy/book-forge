09725bb23e12a04d0c6a715b4c7de06a580141c0
Yevhenii Sukhominskiy <114820378+yevheniisukhominskiy@users.noreply.github.com>
2025-03-23 17:32:00 +0200

    Added controller for work with account

---
+++ src/main/java/by/eugene/bookforge/controllers/MemberController.java
@@ -0,0 +1,41 @@
+package by.eugene.bookforge.controllers;
+
+import by.eugene.bookforge.dto.MemberDTO;
+import by.eugene.bookforge.models.Member;
+import by.eugene.bookforge.services.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/account")
+public class MemberController {
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping("/register")
+    private ResponseEntity<Member> registration(@RequestBody MemberDTO member) {
+        return ResponseEntity.ok(memberService.create(member));
+    }
+
+    @GetMapping("/get/{id}")
+    private ResponseEntity<Member> get(@PathVariable Long id) {
+        return ResponseEntity.ok(memberService.read(id));
+    }
+
+    @PatchMapping("/edit/{id}")
+    private ResponseEntity<Member> edit(@RequestBody Member member) {
+        return ResponseEntity.ok(memberService.update(member));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    private HttpStatus delete(@PathVariable Long id) {
+        memberService.delete(id);
+        return HttpStatus.OK;
+    }
+}
