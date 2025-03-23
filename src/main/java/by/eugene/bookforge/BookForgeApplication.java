de8f98d0ebddde9672f5fc709bba98fe36ee0b72
Yevhenii Sukhominskiy <114820378+yevheniisukhominskiy@users.noreply.github.com>
2025-03-23 14:55:23 +0200

    Created a main structure

---
+++ src/main/java/by/eugene/bookforge/BookForgeApplication.java
@@ -0,0 +1,13 @@
+package by.eugene.bookforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookForgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookForgeApplication.class, args);
+    }
+
+}
