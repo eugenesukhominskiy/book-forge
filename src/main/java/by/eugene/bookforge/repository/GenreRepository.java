92e80151904219eebcc595e5e1a7729d0c7005d1
Yevhenii Sukhominskiy <114820378+yevheniisukhominskiy@users.noreply.github.com>
2025-03-24 21:11:17 +0200

    Add Genre functionality

---
+++ src/main/java/by/eugene/bookforge/repository/GenreRepository.java
@@ -0,0 +1,9 @@
+package by.eugene.bookforge.repository;
+
+import by.eugene.bookforge.models.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+}
